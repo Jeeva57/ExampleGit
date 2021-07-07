524163e53f98a3216eb82b160c5a3c3820ec71f3
Jeeva57 <jeeva.mechengr@gmail.com>
2021-07-07 12:12:17 +0530

    FaceBook login

--- src/GitPrac.java
+++ src/GitPrac.java
@@ -1,10 +1,15 @@
 
 public class GitPrac {
-	public static void main(String[] args) {
+	
+		public static void FacebookLogin() {
+			
+			System.out.println("Raju Fuck boy");
+		}
 		
 		
-		
-		
-	}
+		public static void main(String[] args) {
+			FacebookLogin();
+		}
+	
 
 }
